555ce3a19c0c485256f8091412854eaa08222e90
lsh02002 <lsh02002@hanmail.net>
2025-01-15 15:01:21 +0900

    fix baseUrl

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://15.165.128.174:8090";
-export const BASE_URL = "http://localhost:8090";
+export const BASE_URL = "http://15.165.128.174:8090";
+// export const BASE_URL = "http://localhost:8090";
