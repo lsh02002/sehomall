0b5285e6c9169f986536735f4d055b7cb2cd634b
lsh02002 <lsh02002@hanmail.net>
2025-01-26 17:02:36 +0900

    Update StarRating.js

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -2,13 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import { FaStar } from "react-icons/fa";
 
-const createArray = (length) => [...Array(length)];
-
 const StarRating = ({ totalStars }) => {
+  const createArray = (length) => [...Array(length)];
+
   return (
     <Container>
       {createArray(totalStars).map((i) => (
         <FaStar key={i} color="gold" />
+      ))}
+      {createArray(5 - totalStars).map((i) => (
+        <FaStar key={i} color="rgb(216, 216, 216)" />
       ))}
     </Container>
   );
@@ -19,5 +22,5 @@ export default StarRating;
 const Container = styled.div`
   width: 100px;
   text-align: left;
-  border-radius: 5px;  
+  border-radius: 5px;
 `;
